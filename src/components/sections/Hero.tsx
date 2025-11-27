2f44d9172c0c80d2a7f2ac8a80e8e63246adea19
Charles <Charles.liwananda@gmail.com>
2025-11-26 18:59:31 -0700

    commit

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -75,7 +75,7 @@ export const Hero = () => {
 
         <div className="flex flex-1 lg:w-1/2 lg:h-auto relative lg:max-w-non lg:mx-0 mx-auto max-w-3xl">
           <img
-            src="https://images.pexels.com/photos/7773731/pexels-photo-7773731.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            src="../../../public/assets/Ai.jpg"
             alt="Hero image"
             width={2350}
             height={2359}
